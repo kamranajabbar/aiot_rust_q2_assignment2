780ff32f22d628f9d07388003a6684010d9d3cd7
Kamran jabbar <kamranajabbar@gmail.com>
2019-12-09 11:07:31 +0500

    updated

--- src/q2.rs
+++ src/q2.rs
@@ -4,16 +4,16 @@ use std::io::prelude::*;
 pub fn run() {
 
     loop {
-        print!(">");
+        print!(">>>");
         io::stdout().flush().ok();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        io::stdin().read_line(&mut input).expect("Invalid input");
         let input_val = input.trim();
 
         println!("{}", input_val);
 
         io::stdout().flush().ok();
     }
-    
+
 }
